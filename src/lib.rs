25e6b5f1a2fa99537fc5378f0dc874cbcd07f307
Lachezar Lechev <elpiel93@gmail.com>
2024-11-28 09:03:02 +0200

    feat: enable async support with feature
    Signed-off-by: Lachezar Lechev <elpiel93@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,25 @@
 pub mod accel;
 pub mod address;
 pub mod calibration;
+#[cfg(feature = "async")]
 pub mod calibration_async;
 pub mod calibration_blocking;
 pub mod clock_source;
 pub mod config;
 mod dmp_firmware;
 pub mod error;
+#[cfg(feature = "async")]
 pub mod error_async;
 pub mod euler;
 pub mod fifo;
 mod firmware_loader;
+#[cfg(feature = "async")]
 mod firmware_loader_async;
 pub mod gravity;
 pub mod gyro;
 pub mod quaternion;
 pub mod registers;
 pub mod sensor;
+#[cfg(feature = "async")]
 pub mod sensor_async;
 pub mod yaw_pitch_roll;
